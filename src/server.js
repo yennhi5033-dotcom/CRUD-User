f0881b6c5deb3c23d90dbc9eb49dbd37e61809d1
Nguyen Thi Yen Nhi <nguyentyennhi90@dtu.edu.vn>
2026-09-13 16:21:05 +0700

    feat: add allowed origin for production server in CORS configuration

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
 
-
+  "https://crud-user-1lmh.onrender.com",
   // Vercel frontend
   "https://fe-task-management-flame.vercel.app",
 ];
